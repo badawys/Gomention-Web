956c4f3e8c01abaa46be716a005d3c9ee9ecc282
Ahmad Elbadawy <elbadawy19793@yahoo.com>
2015-06-25 10:21:56 +0200

    cards re-design

--- resources/views/frontend/user/mention/cards/text.blade.php
+++ resources/views/frontend/user/mention/cards/text.blade.php
@@ -1,7 +1,28 @@
 <div class="panel panel-default">
-    <div class="panel-heading">{!! '<a>'.$mention->data['type'] . '</a>'. ' mention from ' . '<a>'. $mention->by_user->name . '</a> to <a>' . $mention->to_user->name . '</a>' !!}</div>
+
+    @if($mention->by_user->id == Auth::user()->id)
+        <div class="panel-heading">
+            <div class="row" style="margin: 0px; line-height: 60px;">
+                <img class="img-circle" src="{!!asset('img/frontend/ahmad.jpg')!!}" style="width: 60px; height: 60px; margin-right: 10px;">
+                <a>You ‎‎</a> <span style="color: #a9a9a9">▶</span> <a>{{$mention->to_user->name}}</a>
+                <a class="pull-right"><i class="fa  fa-ellipsis-v" style="font-size: 20px; color: #a9a9a9"></i></a>
+            </div>
+        </div>
+    @else
+        <div class="panel-heading">
+            <a>{{$mention->by_user->name}}</a>
+        </div>
+    @endif
 
     <div class="panel-body">
         {{$mention->data['text']}}
     </div>
+
+    <div class="panel-footer" style="background-color: #ffffff">
+        <i class="fa fa-file-text-o"></i>
+        <div class="pull-right">
+            {{$mention->created_at->diffForHumans()}}
+        </div>
+    </div>
+
 </div>
