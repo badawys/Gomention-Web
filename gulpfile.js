9efe5b16f7e5b441eac7b9399065ad064428f9d0
Ahmad Elbadawy <elbadawy19793@yahoo.com>
2016-03-14 13:36:20 +0200

    APIs Docs added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var elixir = require('laravel-elixir');
+require('laravel-elixir-apidoc');
 
 elixir(function(mix) {
     mix
-
         .sass([ // Process front-end stylesheets
                 'frontend/main.scss',
                 'frontend/jasny_bootstrap/jasny-bootstrap.scss'
@@ -37,44 +37,11 @@ elixir(function(mix) {
         .copy('vendor/twbs/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts')
 
         // Apply version control
-        .version(["css/frontend.css", "js/frontend.js", "css/backend.css", "js/backend.js"]);
+        .version(["css/frontend.css", "js/frontend.js", "css/backend.css", "js/backend.js"])
+
+        .apidoc({
+            src: "app/Http/Routes/API/",
+            dest: "docs/api",
+            debug: true
+        });
 });
-
-/**
- * Uncomment for LESS version
- */
-/*elixir(function(mix) {
-    mix
-        // Copy webfont files from /vendor directories to /public directory.
-        .copy('vendor/fortawesome/font-awesome/fonts', 'public/fonts')
-        .copy('vendor/twbs/bootstrap/fonts', 'public/fonts')
-
-        .less([ // Process front-end stylesheets
-            'frontend/main.less'
-        ], 'resources/assets/css/frontend')
-        .styles([  // Combine pre-processed CSS files
-            'frontend/main.css'
-        ], 'public/css/frontend.css', 'resources/assets/css')
-        .scripts([ // Combine front-end scripts
-            'plugins.js',
-            'frontend/main.js'
-        ], 'public/js/frontend.js', 'resources/assets/js')
-
-        .less([ // Process back-end stylesheets
-            'backend/AdminLTE.less',
-        ], 'resources/assets/css/backend')
-        .styles([ // Combine pre-processed CSS files
-            'bootstrap.css',
-            'font-awesome.css',
-            'backend/main.css',
-            'backend/skin.css'
-        ], 'public/css/backend.css', 'resources/assets/css')
-        .scripts([ // Combine back-end scripts
-            'plugins.js',
-            'backend/main.js'
-        ], 'public/js/backend.js', 'resources/assets/js')
-
-        // Apply version control
-        .version(["css/frontend.css", "js/frontend.js", "css/backend.css", "js/backend.js"]);
-});
-*/
