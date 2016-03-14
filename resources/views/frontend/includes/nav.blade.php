a759d67ea44b6740da3d212f1a58e1c468a3386c
Ahmad Elbadawy <elbadawy19793@yahoo.com>
2016-03-14 15:26:57 +0200

    "Add mention button" UX improvement

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -38,26 +38,26 @@
 		</div>
 	</nav>
 
-    <nav id="myNavmenu" class="navmenu navmenu-default navmenu-fixed-left offcanvas" role="navigation">
-        <ul class="nav navmenu-nav">
-            @if (Auth::guest())
-                <li>{!! link_to('auth/login', 'Login') !!}</li>
-                <li>{!! link_to('auth/register', 'Register') !!}</li>
-            @else
-                <li>{!! link_to('dashboard', 'Dashboard') !!}</li>
-                <li>{!! link_to_route('profile.show', 'Show Profile' , Auth::user()->id) !!}</li>
-                <li>{!! link_to('auth/password/change', 'Change Password') !!}</li>
-                @permission('view_admin_link')
-                <li role="presentation" class="divider"></li>
+    {{--<nav id="myNavmenu" class="navmenu navmenu-default navmenu-fixed-left offcanvas" role="navigation">--}}
+        {{--<ul class="nav navmenu-nav">--}}
+            {{--@if (Auth::guest())--}}
+                {{--<li>{!! link_to('auth/login', 'Login') !!}</li>--}}
+                {{--<li>{!! link_to('auth/register', 'Register') !!}</li>--}}
+            {{--@else--}}
+                {{--<li>{!! link_to('dashboard', 'Dashboard') !!}</li>--}}
+                {{--<li>{!! link_to_route('profile.show', 'Show Profile' , Auth::user()->id) !!}</li>--}}
+                {{--<li>{!! link_to('auth/password/change', 'Change Password') !!}</li>--}}
+                {{--@permission('view_admin_link')--}}
+                {{--<li role="presentation" class="divider"></li>--}}
                 {{-- This can also be @role('Administrator') instead --}}
-                <li>{!! link_to_route('backend.dashboard', 'Administration') !!}</li>
-                <li>{!! link_to_route('Logs', 'Logs') !!}</li>
-                @endpermission
-                <li role="presentation" class="divider"></li>
-                <li>{!! link_to('auth/logout', 'Logout') !!}</li>
-            @endif
-        </ul>
-    </nav>
+                {{--<li>{!! link_to_route('backend.dashboard', 'Administration') !!}</li>--}}
+                {{--<li>{!! link_to_route('Logs', 'Logs') !!}</li>--}}
+                {{--@endpermission--}}
+                {{--<li role="presentation" class="divider"></li>--}}
+                {{--<li>{!! link_to('auth/logout', 'Logout') !!}</li>--}}
+            {{--@endif--}}
+        {{--</ul>--}}
+    {{--</nav>--}}
 
     <!-- Modal -->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -65,19 +65,37 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Mention This Button</h4>
+                    <h4 class="modal-title" id="myModalLabel">Add mention button to your browser</h4>
                 </div>
                 <div class="modal-body">
                     <p>
-                        To mention your friends, you need to drag "Mention This" button to your browser's bookmarks bar. Jsut that easy, drag it and Gomention ;)
+                        To mention your friends, you need to drag "MENTION" button to your browser's bookmarks bar. Jsut that easy, drag it and Gomention ;)
                     </p>
-                    <a type="button" class="btn btn-primary btn-block" href="javascript: ((function() {    if (window.location.protocol != 'http:' && window.location.protocol != 'https:') {        alert('This page cannot be mentioned.');        return;    }    var url = 'http://alpha-tests.gomention.com/mention-this?url=' + encodeURIComponent(window.location.href);    window.open(url,'Mention This','width=420, height=470');})())">Mention This</a>
-                    <p style="font-size: 10px; margin-top: 10px;">
-                        To show your bookmarks bar: click (Ctrl+Shift+B) in most browsers.
-                    </p>
-                    <p style="font-size: 10px;">
-                        Tested in: (Chrome 47+) (Firefox 42+)
-                    </p>
+                    <a type="button" class="btn btn-primary" style="margin: 0 auto; display: block; width: 150px;" href="javascript: ((function() {    if (window.location.protocol != 'http:' && window.location.protocol != 'https:') {        alert('This page cannot be mentioned.');        return;    }    var url = 'http://alpha-tests.gomention.com/mention-this?url=' + encodeURIComponent(window.location.href);    window.open(url,'Mention This','width=420, height=470');})())">Mention</a>
+                    <ul style="font-size: 10px;margin-top: 40px;padding-top: 10px;border-top: 1px solid #ddd;">
+                        <li>
+                            To show your bookmarks bar: click (Ctrl+Shift+B) in most browsers.
+                        </li>
+                        <li>
+                            To show your bookmarks bar in firefox:
+                            <ul style="list-style: decimal; font-size: 9px;">
+                                <li>
+                                    Click the menu button and choose Customize.
+                                </li>
+                                <li>
+                                    Click the Show / Hide Toolbars dropdown menu at the bottom of the screen and choose the items you want to display.
+                                </li>
+                                <li>
+                                    Click the green Exit Customize button.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Tested in: (Chrome 47+) (Firefox 42+)
+                        </li><li>
+                            Microsoft Edge is not supported.
+                        </li>
+                    </ul>
                 </div>
                 <div class="modal-footer">
                 </div>
